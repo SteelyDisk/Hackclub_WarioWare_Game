5720f1f8d9c1b9d1bf8a3b10e2477072df8acb76
SteelyDisk <amarihobson55@gmail.com>
2026-08-03 15:12:44 -0400

    Created the basic game mechanic

--- hand.gd
+++ hand.gd
@@ -5,6 +5,7 @@ var stp = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	animated_sprite.flip_h = true
 	animation_loop()
 
 
--- minigame_3.gd
+++ minigame_3.gd
@@ -1,11 +1,47 @@
 extends Node2D
-@onready var animated_sprite: AnimatedSprite2D = $"Hand"
+@onready var animated_sprite: AnimatedSprite2D = $Hand
+@onready var enemy: AnimatedSprite2D = $Enemy
+
+var win = false
+var lose = false
+
+func _game(event: InputEvent):
+	if Input.is_action_just_pressed("ui_accept") or event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		if animated_sprite.animation == "Move_0" and enemy.animation == "Move_2" and $Hand.is_playing() and $Enemy.is_playing():
+			win = true
+			
+		elif animated_sprite.animation == "Move_0" and enemy.animation == "Move_1" and $Hand.is_playing() and $Enemy.is_playing():
+			lose = true
+			
+		elif animated_sprite.animation == "Move_1" and enemy.animation == "Move_0" and $Hand.is_playing() and $Enemy.is_playing():
+			win = true
+			
+		elif animated_sprite.animation == "Move_1" and enemy.animation == "Move_2" and $Hand.is_playing() and $Enemy.is_playing():
+			lose = true
+			
+		elif animated_sprite.animation == "Move_2" and enemy.animation == "Move_1" and $Hand.is_playing() and $Enemy.is_playing():
+			win = true
+			
+		elif animated_sprite.animation == "Move_2" and enemy.animation == "Move_0" and $Hand.is_playing() and $Enemy.is_playing():
+			lose = true
+			
+
+func win_lose_system():
+	if lose == true:
+		print("can you see this? Good.")
+		Global.lives -= 1
+		Global.minigames_done -= 1
+		get_tree().change_scene_to_file("res://level_scene.tscn")
+	elif win == true:
+		print("can you see this? Good.")
+		get_tree().change_scene_to_file("res://done_screen.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	while true:
+		win_lose_system()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	animated_sprite.play
+func _process(_delta: float) -> void:
+	pass
